dc16b029adb2e8d059c4f977b8017fbbf0217fdd
Christopher Froehlich <cfroehlich@doctorevidence.com>
2015-05-20 23:27:39 -0400

    Added slack/deploy gulp task.

---
+++ gulp/tasks/run.js
@@ -0,0 +1,27 @@
+﻿var run = require('gulp-run');
+var gulp = require('gulp');
+var argv = require('yargs').argv;
+var _slack = require('node-slack');
+var slack = new _slack('https://hooks.slack.com/services/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');
+var through = require('through2');
+
+var message = {
+    token: 'KpnhJmwZLEav6suDAG90B8bf',
+    team: 'cabreraresearch',
+    channel: '#tech',
+    icon_emoji: ':bowtie:',
+    username: 'ibid'
+};
+
+var sendToSlack = function(i) {
+    slack.send(i);
+    return through.obj(i);
+};
+
+// Use gulp-run to start a pipeline 
+gulp.task('deploy', function () {
+    var p = argv.message;
+    message.text = 'Just deployed new release that: ' + p;
+    run('firebase deploy -m ' + p).exec() // prints "Hello World\n". 
+        .pipe(sendToSlack(message));
+})
