536e996672c90a64ccfa48f108de16ac85180811
Christopher Froehlich <christopher@crlab.us>
2015-10-06 19:24:29 -0400

    #52. Clicking a notification automatically archives it.

--- src/js/tags/menu/meta-notifications.js
+++ src/js/tags/menu/meta-notifications.js
@@ -1,6 +1,7 @@
 const riot = require('riot');
 const $ = require('jquery')
 require('bootstrap-hover-dropdown')
+const moment = require('moment')
 
 const CONSTANTS = require('../../constants/constants')
 require('../../tools/shims')
@@ -20,17 +21,18 @@ const html = `<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdo
                     </li>
                     <li>
                         <ul class="dropdown-menu-list scroller" style="height: 250px;" data-handle-color="#637283">
-                            <li if="{ notifications }"
-                                each="{ notifications }"
+                            <li if="{ true != archived }"
+                                each="{ val, i in notifications }"
                                 onclick="{ parent.onClick }">
-                                <a href="javascript:;">
-                                    <span class="time">{ time }</span>
-                                    <span class="details">
-                                        <span class="label label-sm label-icon label-success">
-                                            <i class="fa fa-plus"></i>
-                                        </span>
-                                        { event }
+                                <a>
+                                    <span if="{ val.photo }" class="photo">
+										<img src="{ val.photo }" class="img-circle" alt="">
                                     </span>
+                                    <span class="subject">
+										<span class="from">{ val.from }</span>
+										<span class="time" style="padding: 0;">{ parent.getTime(val.time) }</span>
+                                    </span>
+                                    <span class="message">{ val.event }</span>
                                 </a>
                             </li>
                         </ul>
@@ -41,23 +43,39 @@ const html = `<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdo
 riot.tag('meta-notifications', html, function (opts) {
 
     const MetaMap = require('../../../MetaMap.js');
+    const fbPath = CONSTANTS.ROUTES.NOTIFICATIONS.format(MetaMap.User.userId)
 
     this.notifications = [];
     this.allNotifications = [];
 
     this.onClick = (event, params) => {
-        console.log(event, params);
+        let item = event.item.val
+        MetaMap.MetaFire.setData(true, `${fbPath}/${item.id}/archive`)
         return true;
     }
 
+    this.getTime = (time) => {
+        return moment(new Date(time)).fromNow()
+    }
+
     this.on('mount', () => {
-        MetaMap.MetaFire.on(CONSTANTS.ROUTES.NOTIFICATIONS.format(MetaMap.User.userId), (data) => {
-            this.allNotifications = data;
-            this.notifications = _.filter(_.sortBy(this.allNotifications, 'date'), (d) => {
-                var include = d.archive != true;
-                return include;
-            });
-            this.update();
-        });
+        MetaMap.MetaFire.getData(fbPath)
+            .then((data) => {
+                if (!data) {
+                    MetaMap.MetaFire.pushData({
+                        event: 'You signed up for MetaMap!',
+                        time: `${new Date() }`,
+                        archive: false
+                    }, fbPath)
+                }
+                MetaMap.MetaFire.on(CONSTANTS.ROUTES.NOTIFICATIONS.format(MetaMap.User.userId), (data) => {
+                    this.allNotifications = _.map(data, (n, id) => { n.id = id; return n;  });
+                    this.notifications = _.filter(_.sortBy(this.allNotifications, 'date'), (d) => {
+                        var include = d.archive != true;
+                        return include;
+                    });
+                    this.update();
+                });
+            })
     });
 });
