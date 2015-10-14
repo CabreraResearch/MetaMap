f05a406c84020766bc2e8aeb3ac871ca06e1e533
sporritt <simon.porritt@gmail.com>
2015-10-15 00:05:48 +1100

    add child node ordering concept in preparation for issue #49

--- src/js/canvas/layout.js
+++ src/js/canvas/layout.js
@@ -2,34 +2,56 @@
 * Custom layout for metamap. Extends the Spring layout. After Spring runs, this
 * layout finds 'part' nodes and aligns them underneath their parents. The alignment
 * - left or right - is set in the parent node's data, as `partAlign`.
+*
+* Layout can be suspended on a per-node basis by setting `suspendLayout` in the Node's
+* data.
+*
+* Child nodes 
 */
 ;(function() {
+	
+	function childNodeComparator(c1, c2) {
+		if (c2.data.order == null) return -1;
+		if (c1.data.order == null) return 1;
+		return c1.data.order < c2.data.order ? -1 : 1;
+	}
 
   jsPlumbToolkit.Layouts["metamap"] = function() {
     jsPlumbToolkit.Layouts.Spring.apply(this, arguments);
 
-    var _oneSet = function(parent, params) {
+    var _oneSet = function(parent, params, toolkit) {
       params = params || {};
       var padding = params.partPadding || 20;
-      if (parent.data.children) {
+      if (parent.data.children && parent.data.suspendLayout !== true) {
 
         var c = parent.data.children,
+		  	childNodes = _.map( c, toolkit.getNode ),
             parentPos = this.getPosition(parent.id),
             parentSize = this.getSize(parent.id),
             magnetizeNodes = [ parent.id ],
             align = (parent.data.partAlign || "right") === "left" ? 0 : 1,
             y = parentPos[1] + parentSize[1] + padding;
+		
+		// sort nodes	
+		childNodes.sort(childNodeComparator);
+		// and run through them and assign order; any that didn't previously have order will get order
+		// set, and any that had order will retain the same value.
+		_.each(childNodes, function(cn, i) {
+			cn.data.order = i;
+		});
 
-        for (var i = 0; i < c.length; i++) {
-          if(c[i]) {
-            var childSize = this.getSize(c[i]),
-                x = parentPos[0] + (align * (parentSize[0] - childSize[0]));
+		for (var i = 0; i < childNodes.length; i++) {
+			var cn = childNodes[i];
+            if(cn) {
+              var childSize = this.getSize(cn.id),
+                  x = parentPos[0] + (align * (parentSize[0] - childSize[0]));
 
-            this.setPosition(c[i], x, y, true);
-            magnetizeNodes.push(c[i]);
-            y += (childSize[1] + padding);
-          }
-        }
+              this.setPosition(cn.id, x, y, true);
+              magnetizeNodes.push(cn.id);
+              y += (childSize[1] + padding);
+            }
+		}
+          
 
       }
     }.bind(this);
@@ -46,7 +68,7 @@
         // a million ways of determining what is a Part node...here I just use
         // a rudimentary construct in the data)
         if (n.data.parent == null) {
-          _oneSet(n, params);
+          _oneSet(n, params, toolkit);
         }
       }
 
