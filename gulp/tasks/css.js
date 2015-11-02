2cf91897a8c2551f3f8a3a448284fe8636e78a5d
Christopher Froehlich <christopher@crlab.us>
2015-11-02 17:48:44 -0500

    #58. Fixed styling issues introduced within the last few commits.

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -2,15 +2,35 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
+var runSequence = require('run-sequence');
 
-
+gulp.task('css', function (cb) {
+    return runSequence(
+        'meta-css',
+        'vendor-css',
+        function (error) {
+            if (error) {
+                console.log(error.message);
+            }
+            cb(error);
+        });
+});
 
 // create task
-gulp.task('css', function(){
-    return gulp.src(['src/css/**/*.css', 'src/vendor/**/*.css'])
+gulp.task('meta-css', function(){
+    return gulp.src('src/css/**/*.css')
         .pipe(concat('MetaMap.css'))
         .pipe(gulp.dest('dist'))
         .pipe(minifyCSS())
         .pipe(concat('MetaMap.min.css'))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('vendor-css', function () {
+    return gulp.src('src/vendor/**/*.css')
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest('dist'))
+        .pipe(minifyCSS())
+        .pipe(concat('vendor.min.css'))
         .pipe(gulp.dest('dist'))
 });
