c2a960d4673d505bf1bcd213d2f46fb0abedd68e
Christopher Froehlich <christopher@crlab.us>
2015-11-09 15:37:09 -0500

    Added cache busting to HTML pages.

---
+++ gulp/tasks/cache.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var replace = require('gulp-replace')
+var rev = require('gulp-rev-append')
+var rename = require("gulp-rename")
+
+gulp.task('cacheBust', function () {
+    gulp.src('**/_*.html')
+        .pipe(rev())
+        .pipe(rename(function (path) {
+            path.basename = path.basename.slice(1)
+        }))
+        .pipe(gulp.dest('./'));
+});
