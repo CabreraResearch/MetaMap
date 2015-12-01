a62a55fd4c58dd8f2c3cdf36e8337efd61f3589e
Christopher Froehlich <christopher@crlab.us>
2015-12-01 12:26:47 -0500

    #19. Another version no sync issue.

--- gulp/tasks/git.js
+++ gulp/tasks/git.js
@@ -1,28 +1,28 @@
 var gulp = require('gulp')
 var git = require('gulp-git')
-var pkg = require('../../package.json')
 var argv = require('yargs').argv
 
 // Run git push
 // remote is the remote repo
 // branch is the remote branch to push to
-gulp.task('commit', function(){
-  var p = argv.message;
-  return gulp.src('./')
-    .pipe(git.add())
-    .pipe(git.commit(p))
+gulp.task('commit', function () {
+    var p = argv.message;
+    return gulp.src('./')
+        .pipe(git.add())
+        .pipe(git.commit(p))
 });
 
-gulp.task('push', function(){
-  git.push('origin', 'master', function (err) {
-    if (err) throw err;
-  });
+gulp.task('push', function () {
+    git.push('origin', 'master', function (err) {
+        if (err) throw err;
+    });
 });
 
 // Tag the repo with a version
 gulp.task('tag', function () {
-  var p = argv.message;
-  git.tag('v'+pkg.version, p, function (err) {
-    if (err) throw err;
-  });
+    var pkg = require('../../package.json')
+    var p = argv.message;
+    git.tag('v' + pkg.version, p, function (err) {
+        if (err) throw err;
+    });
 });
